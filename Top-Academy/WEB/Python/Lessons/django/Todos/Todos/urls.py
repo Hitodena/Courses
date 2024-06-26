f9b01ca8a55cf8e90a5630f4ada77a662602cd28
Hitodena <Hitodena@ya.ru>
2024-06-26 21:53:00 +0300

    Top-Academy: WEB: Python: Lessons: django

--- Top-Academy/WEB/Python/Lessons/django/Todos/Todos/urls.py
+++ Top-Academy/WEB/Python/Lessons/django/Todos/Todos/urls.py
@@ -14,19 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from todo import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    
+    path("admin/", admin.site.urls),
     # Auth
-    path('signup/', views.signupuser, name='signupuser'),
-    path('logout/', views.logoutuser, name='logoutuser'),
-    path('login/', views.loginuser, name='loginuser'),
-
+    path("signup/", views.signupuser, name="signupuser"),
+    path("logout/", views.logoutuser, name="logoutuser"),
+    path("login/", views.loginuser, name="loginuser"),
     # Todos
-    path('current/', views.currenttodos, name='currenttodos'),
-    path('', views.home, name='home'),
+    path("current/", views.currenttodos, name="currenttodos"),
+    path("", views.home, name="home"),
+    path("create/", views.createtodo, name="createtodo"),
+    path("todo/<int:todo_pk>/", views.viewtodo, name="viewtodo"),
+    path("todo/<int:todo_pk>/complete", views.completetodo, name="completetodo"),
+    path("todo/<int:todo_pk>/delete", views.completetodo, name="deletetodo"),
+    path("completed/", views.completedtodos, name="completedtodos"),
 ]
